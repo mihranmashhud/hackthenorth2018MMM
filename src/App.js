7299421d19a4160403cb681a3f26eff8682d6cc9
Mihran Mashhud <mihranmashhud@gmail.com>
2018-09-15 10:14:39 -0400

    first build (again)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Grid from './components/Grid'
+import Grid from './components/Grid';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        
+        <Grid />
       </React.Fragment>
     );
   }
