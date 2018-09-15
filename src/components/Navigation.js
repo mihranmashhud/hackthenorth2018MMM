8b9347243aa1a2c6ada858d4251a9c6b75e361a1
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2018-09-15 19:05:32 -0400

    Fixed Navigation stuff

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,29 @@
 import React, { Component } from "react";
+import { Button, Navbar, Nav, NavItem } from "react-bootstrap";
 
 export default class Navigation extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navigation>
-          <Navigation.Header>
-            <Navigation.Brand>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
               <a href="#brand">Github Gallery</a>
-            </Navigation.Brand>
-          </Navigation.Header>
-          <Navigation.Collapse>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
             <Nav pullRight>
               <NavItem eventKey={1} href="#">
                 +
               </NavItem>
               <NavItem eventKey={2} href="#">
-                Account
-              </NavItem>
-              <NavItem eventKey={3} href="#">
-                Login
+                Sign In
               </NavItem>
             </Nav>
-          </Navigation.Collapse>
-        </Navigation>
+          </Navbar.Collapse>
+        </Navbar>
+        ;
       </React.Fragment>
     );
   }
