08a7f3fbcd9dccf7214ec528fb1844be98511466
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2018-09-15 18:09:04 -0400

    COMMIT!!!

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navigation>
+          <Naviagation.Header>
+            <Navigation.Brand>
+              <a href="#brand">Home Page</a>
+            </Navigation.Brand>
+          </Naviagation.Header>
+        </Navigation>
+      </React.Fragment>
+    );
+  }
+}
