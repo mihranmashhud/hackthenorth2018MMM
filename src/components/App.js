f601b3f09deabe16393125dbb97bfdf3a35ea1ed
Mihran Mashhud <mihranmashhud@gmail.com>
2018-09-15 17:02:05 -0400

    more restructuring

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { BrowseRouter as Router } from "react-router-dom";
+import Home from "./Home";
 
 class App extends Component {
   render() {
-    return <React.Fragment />;
+    return (
+      <Router>
+        <Home />
+      </Router>
+    );
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ViewGrid from "./ViewGrid";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ViewGrid />
+      </React.Fragment>
+    );
+  }
+}
