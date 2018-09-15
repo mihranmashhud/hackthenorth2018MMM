44c87f48f16824c180ff45236f2d0e518e5ef6ce
Mrugank <mrugank2@gmail.com>
2018-09-15 11:10:05 -0400

    Changed message to Lorem Ipsum

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,10 +4,8 @@ export default class Description extends Component {
   render() {
     return (
       <React.Fragment>
-          <p> This is the BS stuff that IB students put on their essays and stuff to sound smart and act as if they know 
-              what they are doing
-              
-              lore</p>
+          <p> Exercitation reprehenderit velit Lorem in laboris elit adipisicing ex non ipsum culpa. Dolor consectetur aliqua culpa amet excepteur occaecat officia consectetur et. Cupidatat elit deserunt est irure voluptate ut fugiat adipisicing anim irure excepteur sint. Qui voluptate irure in in nostrud.
+          </p> 
       </React.Fragment>
     )
   }
