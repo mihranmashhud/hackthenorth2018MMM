133522f3cb017e04a7a86c59bcffab41c9064595
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2018-09-15 11:20:29 -0400

    Title added

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Description from './Description'
+import Title from './Title';
 
 export default class Showcase extends Component {
   render() {
     return (
       <React.Fragment>
+        <Title/>
         <Description />
       </React.Fragment>
     )
