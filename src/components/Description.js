8992d23783cf3f663526ff5e4ebb73016f6176f6
Mrugank <mrugank2@gmail.com>
2018-09-15 11:55:19 -0400

    Moved Description Component to Showcase

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import styled from "styled-components"
+
+const FadedDesc = styled.p`
+  position: absolute;
+  border: 30px;
+  padding: 30px;
+  color: red;
+
+`
 
 export default class Description extends Component {
   render() {
     return (
       <React.Fragment>
-          <p> Exercitation reprehenderit velit Lorem in laboris elit adipisicing ex non ipsum culpa. Dolor consectetur aliqua culpa amet excepteur occaecat officia consectetur et. Cupidatat elit deserunt est irure voluptate ut fugiat adipisicing anim irure excepteur sint. Qui voluptate irure in in nostrud.
-          </p> 
+          <FadedDesc> Exercitation reprehenderit velit Lorem in laboris elit adipisicing ex non ipsum culpa. Dolor consectetur aliqua culpa amet excepteur occaecat officia consectetur et. Cupidatat elit deserunt est irure voluptate ut fugiat adipisicing anim irure excepteur sint. Qui voluptate irure in in nostrud.
+          </FadedDesc> 
       </React.Fragment>
     )
   }
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
-import Description from './Description'
+import React, {
+  Component
+} from 'react'
+
+const FadedDesc = styled.p `
+  position: absolute;
+  border: 30px;
+  padding: 30px;
+  color: red;
+
+`
+
 
 export default class Showcase extends Component {
   render() {
     return (
       <React.Fragment>
-        <Description />
+          <FadedDesc> Exercitation reprehenderit velit Lorem in laboris elit adipisicing ex non ipsum culpa. Dolor consectetur aliqua culpa amet excepteur occaecat officia consectetur et. Cupidatat elit deserunt est irure voluptate ut fugiat adipisicing anim irure excepteur sint. Qui voluptate irure in in nostrud.
+          </FadedDesc> 
       </React.Fragment>
     )
   }
