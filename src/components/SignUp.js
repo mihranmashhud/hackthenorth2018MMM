79381f5d73cd5630e750878037b96879c6637cd5
Mihran Mashhud <mihranmashhud@gmail.com>
2018-09-15 22:29:47 -0400

    worked on the SignUp page.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,69 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+
+const Wrapper = styled.div`
+  padding: 60px 0;
+`;
+const Form = styled.form`
+  margin: 0 auto;
+  max-width: 320px;
+`;
 
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
-    return <div />;
+    return (
+      <Wrapper>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit">
+            Login
+          </Button>
+        </Form>
+      </Wrapper>
+    );
   }
 }
